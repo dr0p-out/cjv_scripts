2e43c6fa154076170eb43e4056f95d2b379b769e
dropout <198437834+dr0p-out@users.noreply.github.com>
2025-02-11 03:03:57 +0900

    nomed: skeleton for editor window
    Signed-off-by: dropout <198437834+dr0p-out@users.noreply.github.com>

--- nomed.py
+++ nomed.py
@@ -10,11 +10,15 @@ def err(*args, **kwargs):
   print(*args, **kwargs, file=sys.stderr)
 
 try:
-  from PySide6.QtCore import QEvent
-  from PySide6.QtGui import QPalette
+  from PySide6.QtCore import Qt, QEvent, Slot
+  from PySide6.QtGui import QPalette, QShortcut
   from PySide6.QtWebEngineCore import QWebEngineSettings
   from PySide6.QtWebEngineWidgets import QWebEngineView
-  from PySide6.QtWidgets import QApplication
+  from PySide6.QtWidgets import (
+    QApplication,
+    QPushButton, QTextEdit,
+    QVBoxLayout, QWidget
+  )
 except ImportError:
   traceback.print_exc()
   err('^^^ Tip: try installing pyside (if havent)')
@@ -56,9 +60,57 @@ class SucklessWebView(QWebEngineView):
       is_dark
     )
 
+  def closeEvent(self, ev: QEvent):
+    ed.close()  # closes each other
+    QWebEngineView.closeEvent(self, ev)
+
+class NomEdit(QTextEdit):
+  def __init__(self, *args, **kwargs):
+    QTextEdit.__init__(self, *args, **kwargs)
+    self.setStyleSheet('font-family: "Gothic Nguyen"')
+    self.setPlaceholderText('Type here…')
+    self.setAcceptRichText(False)
+    self.textChanged.connect(self.__handle_kb)
+
+  @Slot()
+  def __handle_kb(self):
+    pass
+
+class EditorWidget(QWidget):
+  # no, we want to hold ref of these
+  # pylint: disable=unused-private-member
+  def __init__(self, *args, **kwargs):
+    QWidget.__init__(self, *args, **kwargs)
+    self.__layout = box = QVBoxLayout()
+    self.setLayout(box)
+    self.__btn = btn = QPushButton()
+    btn.setText('保存／書き込み(S)')
+    btn.clicked.connect(self.__do_save)
+    box.addWidget(btn)
+    self.__nom = nom = NomEdit()
+    box.addWidget(nom)
+    self.__keybind = key = QShortcut(self)
+    key.setKey(Qt.Modifier.CTRL | Qt.Key.Key_S)
+    key.activated.connect(self.__on_key_seq)
+    self.setWindowTitle('Nôm Editor')
+
+  def closeEvent(self, ev: QEvent):
+    pv.close()  # closes each other
+    QWidget.closeEvent(self, ev)
+
+  @Slot()
+  def __on_key_seq(self):
+    self.__btn.animateClick()
+
+  @Slot()
+  def __do_save(self):
+    pass
+
 qa = QApplication(sys.argv)
 
 pv = SucklessWebView()
 pv.show()
+ed = EditorWidget()
+ed.show()
 
 sys.exit(qa.exec())
